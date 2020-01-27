b7f9cbb437e04560903bca1567d6101c1d3fdaa5
John Hsu <john.hsu@ubc.ca>
2020-01-27 15:37:36 -0800

    ADD library JWT Framework
    For all things JSON Web Token related, such as generating JSON Web Keys
    and verifying JSON Web Signatures.
    
    web-token/jwt-easy - load keys from JWK and verify JWS
    web-token/jwt-key-mgmt - for generating public/private keys
    
    Also added a /lti/keygen endpoint, which will generate a public and
    private key pair and output it as a JWK into the debug log. This is
    meant for reference only.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use UBC\LTI\Specs\Launch\ToolLaunch;
 
+use Jose\Component\KeyManagement\JWKFactory;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,4 +26,20 @@ Route::namespace('LTI\Launch')->group(function() {
     Route::post('/lti/launch/tool/login', 'ToolLaunchController@login');
     // unlike login, only POST requests are allowed for the auth response
     Route::post('/lti/launch/tool/auth', 'ToolLaunchController@auth');
+});
+
+// sample code for generating a public/private key pair using the JWT Framework
+Route::get('/lti/keygen', function() {
+    $key = JWKFactory::createRSAKey(
+        4096, // Size in bits of the key. We recommend at least 2048 bits.
+        [
+            'alg' => 'RS256',
+            'use' => 'sig',
+            'key_ops' => ['sign', 'verify'],
+            'kty' => 'RSA'
+        ]);
+    Log::debug("Public Only");
+    Log::debug(json_encode($key->toPublic(), JSON_PRETTY_PRINT));
+    Log::debug("Public & Private");
+    Log::debug(json_encode($key, JSON_PRETTY_PRINT));
 });
