e69612d6010214321905a1d1b59350b383e3fec1
John Hsu <john.hsu@ubc.ca>
2020-02-06 17:09:21 -0800

    EDIT PlatformFactory create related PlatformClient
    Platforms need at least one associated client, so we when we create one
    using the PlatformFactory, it should also create an associated
    PlatformClient.
    
    Using the afterCreate callback on the factory since primary id isn't
    given until it's written to the database. afterMake's callback means
    the platform hasn't been written to the database yet so has no id.
    
    Note that the PlatformClientFactory should not be used by itself, as it
    will error out due to not having a platform_id.

--- database/factories/PlatformFactory.php
+++ database/factories/PlatformFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
 use App\Models\Platform;
+use App\Models\PlatformClient;
 
 
 $factory->define(Platform::class, function (Faker $faker) {
@@ -14,3 +15,14 @@ $factory->define(Platform::class, function (Faker $faker) {
         'auth_req_url' => 'https://' . $domain . '/lti/auth'
     ];
 });
+$factory->afterCreating(
+    Platform::class,
+    function($platform, Faker $faker) {
+        // each platform also needs a registered client
+        $platform->clients()
+                 ->save(
+                     factory(PlatformClient::class)
+                         ->create(['platform_id' => $platform->id])
+                 );
+    }
+);
