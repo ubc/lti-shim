0cce082646d3d567e4ea4d154439a98045e360c6
John Hsu <john.hsu@ubc.ca>
2021-04-19 01:55:23 -0700

    ADD DeepLink AuthReqHandler for launch auth req
    Similar to the LoginHandler, AuthReqHandler handles the authorization
    request stage of the LTI launch. The authorization request is sent by
    the target tool to the shim, the handler checks it over and creates its
    own authorization request to the originating platform.
    
    I've realized that the old implementation of the LTI launch was quite
    overzealous in its launch filtering, calling them in every stage on the
    tool side. Since we're directly crafting the messages in login and auth
    req, there's really no need to filter those messages in this new
    implementation. Hopefully, this results in a speedup. The filters will
    need to be brought back into place for the authorization response stage
    that's coming up next.
    
    Note that the nonce we receive from the target tool needs to be stored
    so we can repeat it back in the auth resp message to the target tool.
    While the nonce we generate will need to be checked for validity in the
    auth resp message from the originating platform.
    
    While we ourselves didn't use the lti_message_hint param in login, we
    need to repeat it back if the originating platform did use it.
    Similarly, if the target tool sent us a state, we need to repeat it back
    in the next auth resp stage.
    
    This means that LtiSession should now also store a nonce and a state (if
    given). I've decided not to remove values from LtiSession that have been
    used, to save a database call.
    
    One thing to note is that since we're storing the LtiSession in
    login_hint, errors surrounding login_hint will NOT have the same log
    stream ID as the login stage. This is because the log stream ID is saved
    in LtiSession, so if we can't recover the LtiSession, we do not know
    what stream ID to use, which means a new random ID was assigned to the
    log stream.

--- app/Http/Controllers/LTI/Launch/DeepLinkController.php
+++ app/Http/Controllers/LTI/Launch/DeepLinkController.php
@@ -8,11 +8,13 @@ use App\Http\Controllers\Controller;
 
 use UBC\LTI\Utils\LtiException;
 use UBC\LTI\Specs\DeepLink\LoginHandler;
+use UBC\LTI\Specs\DeepLink\AuthReqHandler;
 
 class DeepLinkController extends Controller
 {
     /**
-     * Send out LTI launch request's first stage, OIDC login
+     * Receive LTI launch request's first stage, OIDC login, from a platform.
+     * Returns an OIDC login to target tool.
      *
      * @param Request $request
      */
@@ -20,6 +22,18 @@ class DeepLinkController extends Controller
     {
         $handler = new LoginHandler($request);
         return $handler->sendLogin();
+    }
+
+    /**
+     * Receive LTI launch request's second stage, auth request, from a tool.
+     * Returns an auth request to originating platform.
+     *
+     * @param Request $request
+     */
+    public function auth(Request $request)
+    {
+        $handler = new AuthReqHandler($request);
+        return $handler->sendAuth();
     }
 
 }
---
+++ lti/Specs/DeepLink/AuthReqHandler.php
@@ -0,0 +1,141 @@
+<?php
+namespace UBC\LTI\Specs\DeepLink;
+
+use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+use League\Uri\Uri;
+
+use UBC\LTI\Utils\LtiException;
+use UBC\LTI\Utils\LtiLog;
+use UBC\LTI\Utils\Param;
+use UBC\LTI\Specs\ParamChecker;
+use UBC\LTI\Specs\Security\Nonce;
+
+use App\Models\LtiSession;
+use App\Models\Platform;
+use App\Models\Tool;
+
+/**
+ * SECOND STAGE of LTI launch, the Authorization Request
+ *
+ * We first receive an auth req from the target tool.
+ * We restore state from LtiSession.
+ * We then send an auth req to the originating platform.
+ */
+class AuthReqHandler
+{
+
+    private LtiLog $ltiLog;
+    private LtiSession $session;
+    private ParamChecker $checker;
+    private Request $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+        $this->ltiLog = new LtiLog('Launch (Auth Req)');
+        $this->checker = new ParamChecker($request->input(), $this->ltiLog);
+        $this->loadSession();
+    }
+
+    /**
+     * Returns the OIDC login parameters we should send to the target tool. This
+     * is the shim acting as a platform.
+     */
+    public function sendAuth(): Response
+    {
+        $this->receiveAuth();
+        $this->ltiLog->info('Tool Side, send auth req.', $this->request);
+        $authReqParams = [
+            // REQUIRED static
+            Param::SCOPE => Param::OPENID,
+            Param::RESPONSE_TYPE => Param::ID_TOKEN,
+            Param::RESPONSE_MODE => Param::FORM_POST,
+            Param::PROMPT => Param::NONE,
+            // REQUIRED dynamic
+            Param::CLIENT_ID => $this->session->platform_client->client_id,
+            Param::NONCE => Nonce::create(),
+            Param::REDIRECT_URI => route('lti.launch.midway'),
+            // Not required by spec, but needed for us to track session
+            Param::STATE => $this->session->createEncryptedId()
+        ];
+        if (isset($this->session->token[Param::LTI_MESSAGE_HINT])) {
+            $authReqParams[Param::LTI_MESSAGE_HINT] =
+                $this->session->token[Param::LTI_MESSAGE_HINT];
+        }
+        return response()->view(
+            'lti/launch/auto_submit_form',
+            [
+                'title' => 'Auth Request',
+                'formUrl' =>
+                        $this->session->platform_client->platform->auth_req_url,
+                'params' => $authReqParams
+            ]
+        );
+    }
+
+    /**
+     * Just validates to see if the OIDC login request we received from the
+     * platform was valid. This is the shim acting as a tool.
+     */
+    private function receiveAuth()
+    {
+        $this->ltiLog->info('Platform Side, recv auth req: ' .
+            json_encode($this->request->input()), $this->request);
+
+        // required params that needs to be present
+        $requiredParams = [
+            Param::REDIRECT_URI,
+            //Param::LOGIN_HINT, // should be already checked by loadSession()
+            Param::NONCE,
+        ];
+        $this->checker->requireParams($requiredParams);
+        // required params that needs to have matching values
+        $requiredValues = [
+            // static values
+            Param::SCOPE => Param::OPENID,
+            Param::RESPONSE_TYPE => Param::ID_TOKEN,
+            Param::RESPONSE_MODE => Param::FORM_POST,
+            Param::PROMPT => Param::NONE,
+            // dynamic values
+            Param::CLIENT_ID => $this->session->tool->client_id
+        ];
+        $this->checker->requireValues($requiredValues);
+
+        // redirect uri, aka auth resp url, needs to match what was configured
+        // for the target tool
+        $expectedRedirectUri = Uri::createFromString(
+            $this->session->tool->auth_resp_url);
+        $actualRedirectUri = Uri::createFromString(
+            $this->request->input(Param::REDIRECT_URI));
+        if ($expectedRedirectUri != $actualRedirectUri) {
+            throw new LtiException($this->ltiLog->msg(
+                'redirect_uri does not match configuration', $this->request));
+        }
+
+        // nonce and state needs to be stored for the next auth resp step
+        $sessionState = $this->session->token;
+        $sessionState[Param::NONCE] = $this->request->input(Param::NONCE);
+        if ($this->request->has(Param::STATE))
+            $sessionState[Param::STATE] = $this->request->input(Param::STATE);
+
+        $this->session->token = $sessionState;
+        $this->session->save();
+    }
+
+    /**
+     * Load the LtiSession from the login_hint param.
+     */
+    private function loadSession()
+    {
+        if (!$this->request->has(Param::LOGIN_HINT))
+            throw new LtiException($this->ltiLog->msg(
+                'Missing login_hint in auth request', $this->request));
+
+        $this->session = LtiSession::decodeEncryptedId(
+            $this->request->input(Param::LOGIN_HINT));
+        $this->ltiLog->setStreamid($this->session->log_stream);
+    }
+}
